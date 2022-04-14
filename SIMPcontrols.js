1b37b5a843bc9d9fda7c31bd298bafbebd45cbd3
Vivek T Ramamoorthy <vivektramamoorthy@gmail.com>
2022-04-14 10:20:49 +0200

    fixed painting bug for desktop and cleaned up

--- SIMPcontrols.js
+++ SIMPcontrols.js
@@ -2,7 +2,7 @@
 // SIMP RUN AND STOP CONTROLS
 
 var SIMPisRunning=false;
-var toggleSIMP = function (e){
+const toggleSIMP = function (e){
     if(SIMPisRunning){
         SIMPisRunning=false;
     SIMPbutton= document.getElementById("SIMPbutton");
@@ -33,46 +33,37 @@ function SIMPupdateloop(){
 
 var designVariableToPaint=0.5;
 
-var whitebox =document.getElementById('whitebox');
-var blackbox =document.getElementById('blackbox');
-var graybox =document.getElementById('graybox');
+const whitebox =document.getElementById('whitebox');
+const blackbox =document.getElementById('blackbox');
+const graybox =document.getElementById('graybox');
 
-whitebox.addEventListener('click',()=>{
+const voidPaintMode = ()=>{
     designVariableToPaint = 0;
     document.querySelectorAll(".box").forEach(elem=> elem.classList.remove("ispicked"))
     whitebox.classList.add("ispicked")
+}
 
-})
-blackbox.addEventListener('click',()=>{
+const solidPaintmMode = ()=>{
     designVariableToPaint = 1;
     document.querySelectorAll(".box").forEach(elem=> elem.classList.remove("ispicked"))
     blackbox.classList.add("ispicked")
-})
-graybox.addEventListener('click',()=>{
-    designVariableToPaint = 1;
+}
+const grayPaintmMode = ()=>{
+    designVariableToPaint = 0.5;
     document.querySelectorAll(".box").forEach(elem=> elem.classList.remove("ispicked"))
     graybox.classList.add("ispicked")
-})
-whitebox.addEventListener('touchdown',()=>{
-    designVariableToPaint = 0;
-    document.querySelectorAll(".box").forEach(elem=> elem.classList.remove("ispicked"))
-    whitebox.classList.add("ispicked")
+}
 
-})
-blackbox.addEventListener('touchdown',()=>{
-    designVariableToPaint = 1;
-    document.querySelectorAll(".box").forEach(elem=> elem.classList.remove("ispicked"))
-    blackbox.classList.add("ispicked")
-})
-graybox.addEventListener('touchdown',()=>{
-    designVariableToPaint = 1;
-    document.querySelectorAll(".box").forEach(elem=> elem.classList.remove("ispicked"))
-    graybox.classList.add("ispicked")
-})
+whitebox.addEventListener('click',voidPaintMode)
+blackbox.addEventListener('click',solidPaintmMode)
+graybox.addEventListener('click',grayPaintmMode)
+
+whitebox.addEventListener('touchdown',voidPaintMode)
+blackbox.addEventListener('touchdown',solidPaintmMode)
+graybox.addEventListener('touchdown',grayPaintmMode)
 
 
-
-var canvas =document.getElementById('shape');
+const canvas =document.getElementById('shape');
 var mouseDownIndicator=false
 
 canvas.addEventListener('mousedown',()=>{
@@ -83,8 +74,10 @@ canvas.addEventListener('mouseup',()=>{
 })
 canvas.addEventListener('mousemove',(event)=>{
     if(mouseDownIndicator){
-    let mousex=event.layerX
-    let mousey=event.layerY
+        let rect = canvas.getBoundingClientRect();
+
+    let mousex=event.layerX-rect.left;
+    let mousey=event.layerY-rect.top;
     let col = Math.floor(mousex/canvas.clientWidth*nelx);
     let row = Math.floor((mousey/canvas.clientHeight)*nely);
 
@@ -93,7 +86,9 @@ canvas.addEventListener('mousemove',(event)=>{
     }
 })
 
-canvas.addEventListener('touchmove',(event)=>{
+
+
+const handleTouch = (event)=>{
     event.preventDefault()
     let rect = canvas.getBoundingClientRect();
     let mousex=event.touches[0].clientX-rect.left;
@@ -103,15 +98,7 @@ canvas.addEventListener('touchmove',(event)=>{
 
     x[row][col]=designVariableToPaint;
     image(sub(1,x))
-})
-canvas.addEventListener('touchstart',(event)=>{
-    event.preventDefault()
-    let rect = canvas.getBoundingClientRect();
-    let mousex=event.touches[0].clientX-rect.left;
-    let mousey=event.touches[0].clientY-rect.top;
-    let col = Math.floor(mousex/canvas.clientWidth*nelx);
-    let row = Math.floor((mousey/canvas.clientHeight)*nely);
+}
 
-    x[row][col]=designVariableToPaint;
-    image(sub(1,x))
-})
+canvas.addEventListener('touchmove',handleTouch)
+canvas.addEventListener('touchstart',handleTouch)
